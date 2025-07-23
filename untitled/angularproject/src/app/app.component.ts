f39172683061923cf28d37346c44c8bc4f8d8519
AlexiaVoina <alexiavoina2004@gmail.com>
2025-07-23 11:02:29 +0300

    navbar is now a component

--- untitled/angularproject/src/app/app.component.ts
+++ untitled/angularproject/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterModule],
+  standalone: true,
+  imports: [RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: true,
 })
 export class AppComponent {}
