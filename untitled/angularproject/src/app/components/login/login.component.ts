662e31ca00d145c0ee95af06f22e1294a579cce3
AlexiaVoina <alexiavoina2004@gmail.com>
2025-07-25 10:42:53 +0300

    simulated log in function

--- untitled/angularproject/src/app/components/login/login.component.ts
+++ untitled/angularproject/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +12,8 @@ import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angula
 })
 export class LoginComponent {
   private readonly formBuilder = inject(NonNullableFormBuilder);
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
   protected readonly loginForm = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
@@ -18,6 +22,8 @@ export class LoginComponent {
   public handleSubmit() {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      this.authService.login();
+      this.router.navigate(['/home']);
     }
   }
 }
