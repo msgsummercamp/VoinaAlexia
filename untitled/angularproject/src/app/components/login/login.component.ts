c6fa0ed77d8ac841539f2086bd214429e5e0dd1b
AlexiaVoina <alexiavoina2004@gmail.com>
2025-07-25 11:34:10 +0300

    made changes

--- untitled/angularproject/src/app/components/login/login.component.ts
+++ untitled/angularproject/src/app/components/login/login.component.ts
@@ -1,8 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+
+type LoginForm = {
+  email: FormControl<string>;
+  password: FormControl<string>;
+};
 
 @Component({
   selector: 'app-login',
@@ -14,9 +25,9 @@ export class LoginComponent {
   private readonly formBuilder = inject(NonNullableFormBuilder);
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
-  protected readonly loginForm = this.formBuilder.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+  protected readonly loginForm: FormGroup<LoginForm> = this.formBuilder.group({
+    email: this.formBuilder.control('', [Validators.required, Validators.email]),
+    password: this.formBuilder.control('', [Validators.required, Validators.minLength(6)]),
   });
 
   public handleSubmit() {
